7ae23852a40510b4d8d704271aa584a2a8cff7ad
king <blackmagic@solutions.com>
2025-11-04 06:12:58 +0000

    ta listando nos comentarios do script oq foi feito, tirando isso teve umas leves metodoslogias negristicas minhas

--- livraria.py
+++ livraria.py
@@ -12,12 +12,12 @@
 # braia: mano bora por essa porra pra exportar e importar a variavel Livraria.
 # bom hoje no dia 03/11/2025 tivemos um papo com nosso cliente.
 # sera necessario exportacao para um planilha excel.
-# restricoes de livros por idade
+# restricoes de livros por idade.( feito )
 # restricoes para livros que nao possuem muitas unidades ex:
 # tem 15 livros iguais 3 deles nao podem ser retirados
-# ou tem so um exemplar logo ele nao pode ser emprestado.
-# codigo para cada livro. (feito)
-# por variavel de qual estante e.
+# ou tem so um exemplar logo ele nao pode ser emprestado. ( feito )
+# codigo para cada livro. ( feito )
+# por variavel de qual estante e. ( feito )
 
 # bibliotecas importadas
 import pyfiglet as pf
@@ -37,7 +37,7 @@ def MostrarLivrosNaLivraria( Livraria: list ):
     iTamanhoDaLivraria = len( Livraria )
     if iTamanhoDaLivraria <= 0:
         pass
-        
+
     # Vamos percorrer a lista agora se ela nao esta vazia.
     for i in range( iTamanhoDaLivraria ):
 
@@ -45,11 +45,11 @@ def MostrarLivrosNaLivraria( Livraria: list ):
         LivroAtual: Livro = Livraria[ i ]
 
         # Pegue o indice do loop e o livro que o corresponde.
-        strDisplayDoLivro: str = f"{ i } - { LivroAtual.strNomeDoLivro }"
+        strDisplayDoLivro: str = f"{ i } - { LivroAtual.strNomeDoLivro } - Genero: { LivroAtual.strGenero } - Idade Minima: { LivroAtual.iIdadeMinima } - Marcacao: { LivroAtual.strRestricao } - Prateleira: { LivroAtual.iPrateleira } - Codigo: { LivroAtual.strCodigoDoLivro }"
 
         if LivroAtual.bEmprestado:
             strDisplayDoLivro += f" - Emprestado para: { LivroAtual.strQuemAdquiriu } { LivroAtual.strDataLancada }"
-            
+
         # Mostre.
         print( strDisplayDoLivro )
 
@@ -69,7 +69,7 @@ def GerarCodigoUnico( Livraria: list, nTamanho: int ):
     while True:
         # Gere o codigo.
         strCodigo: str = ''.join( random.choices( "1234567890abcdefghijklmnopqrstuvwxyz", k = nTamanho ) ).upper( )
-        
+
         bUnico = True
 
         # Check para ver se ja tem algo livro com o mesmo codigo.
@@ -79,7 +79,7 @@ def GerarCodigoUnico( Livraria: list, nTamanho: int ):
             # Se tiver igual roda tudo de novo.
             if LivroAtual.strCodigoDoLivro == strCodigo:
                 bUnico = False
-        
+
         # Esse codigo e unico nao rode dnv.
         if bUnico:
             break
@@ -95,28 +95,55 @@ def InputDeInteiro( strEntrada: str ):
             iValorDeRetorno = int( input( strEntrada ) )
             break
         except:
-            print("Por favor coloque um numero")
+            print( "Por favor coloque um numero" )
 
     return iValorDeRetorno
+
+def InputDeRestricao( strEntrada: str ):
+    # Uh, pelo oq vi da conversa do leo, o xandao e a tamires( nao e minha ex e a bibliotecaria nao fode porra ).
+    # sera feito assim, restricao vermelha nao podera ser emprestado
+    # verde podera ser emprestado.
+    strSaida = ""
+
+    while True:
+        strSaida = input( strEntrada )
+        if strSaida == "vermelho" or strSaida == "Vermelho":
+            break
+        elif strSaida == "verde" or strSaida == "Verde":
+            break
+        else:
+            print( "Essa palavra nao esta no catalogo de restricao" )
+
+    return strSaida.capitalize( )
 
 lLivraria: list = [ ]
 
 class Livro:
-    def __init__( self, NomeDoLivro, DataLancada, Genero, IdadeMinima, Restricao ):
+    def __init__( self, NomeDoLivro, Genero, IdadeMinima, Restricao, Prateleira ):
         self.strNomeDoLivro: str = NomeDoLivro
-        self.strDataLancada: str = DataLancada
         self.strGenero: str = Genero
         self.bEmprestado: bool = False
         self.strQuemAdquiriu: str = ""
+        self.strDataEmprestada: str = ""
         self.strCodigoDoLivro: str = GerarCodigoUnico( lLivraria, 6 )
         self.iIdadeMinima: int = IdadeMinima
         self.strRestricao: str = Restricao
+        self.iPrateleira: int = Prateleira
 
-    def Emprestar( self, NomeDeQuemAdquiriu: str ):
-        if not self.bEmprestado:
+    def Emprestar( self, NomeDeQuemAdquiriu: str, IdadeDeQuemAdquiriu: int ):
+        if not self.bEmprestado and IdadeDeQuemAdquiriu >= self.iIdadeMinima and self.strRestricao != "Vermelho":
             self.strQuemAdquiriu: str = NomeDeQuemAdquiriu
+            self.strDataEmprestada: str = tempo( )
             self.bEmprestado: bool = True
             return True
+
+        elif IdadeDeQuemAdquiriu < self.iIdadeMinima:
+            print( "A fachetaria desse livro nao e permitido para esse aluno" )
+            return False
+
+        elif self.strRestricao:
+            print( "Esse livro nao e emprestavel" )
+            return False
 
         # Debug
         print( "Esse livro ja foi emprestado" )
@@ -129,7 +156,7 @@ class Livro:
             return True
 
         elif self.strQuemAdquiriu != NomeDeQuemAdquiriu:
-            print("Nao foi esse usuario que pegou emprestado")
+            print( "Nao foi esse usuario que pegou emprestado" )
             return False
 
         # Debug
@@ -139,20 +166,16 @@ class Livro:
 def AdicionarLivroNaLivraria( ):
     print( pf.figlet_format( "Adicionar livro" ) )
     strNomeDoLivro = input( "Diga o nome do livro: " )
-    strDataLancada = tempo( )
     strGenero = input( "Diga o genero do livro: " )
-    iIdadeMinima = InputDeInteiro( "Diga a idade minima para adquiriu o livro: " )
-    
-    print( f"Nome do livro: { strNomeDoLivro }" )
+    iIdadeMinima = InputDeInteiro( "Diga a idade minima para adquirir o livro: " )
+    strRestricao = InputDeRestricao( "Diga qual restricao esse livro possui se e vermelho ou verde: " )
+    iPrateleira = InputDeInteiro( "Diga qual prateleira esse livro pertence: " )
 
     # adiciona as informacoes a classe.
-    NovoLivro = Livro( strNomeDoLivro, strDataLancada, strGenero, iIdadeMinima,  )
+    NovoLivro = Livro( strNomeDoLivro, strGenero, iIdadeMinima, strRestricao, iPrateleira )
 
     # adiciona o livro a lista.
     lLivraria.append( NovoLivro )
-
-    # Debug.
-    print( "Adicionar livro pronto!" )
 
 def RemoverLivroNaLivraria( ):
     print( pf.figlet_format( "remover livro" ) )
@@ -177,23 +200,23 @@ def RemoverLivroNaLivraria( ):
             if iEscolha < 0 or iEscolha > iTamanhoDaLivraria:
                 print( "Por favor coloque um dos indices listado" )
                 continue
-            
+
             # Remova o livro.
             lLivraria.pop( iEscolha )
 
             # Ja atingimos nosso objetivo pare o loop.
             break
-        
+
         except Exception as e:
             # Pare o loop caso seja solicitado um interrupt ( CTRL + C ).
             if KeyboardInterrupt == e:
                 break
 
-            print("Por favor coloque um numero")         
+            print("Por favor coloque um numero")
 
 def EmprestarLivroNaLivraria( ):
     print(pf.figlet_format("Emprestar livro"))
-    
+
     # Mostre os livros que tem na livraria.
     MostrarLivrosNaLivraria( lLivraria )
 
@@ -204,6 +227,9 @@ def EmprestarLivroNaLivraria( ):
     if iTamanhoDaLivraria <= 0:
         print( "A livraria ta vazia, por favor adicione algum livro" )
         pass
+
+    # Para o loop broski.
+    bPare = False
 
     # Parte do emprestimo.
     while True:
@@ -214,40 +240,39 @@ def EmprestarLivroNaLivraria( ):
         try:
             # Salve a escolha do usuario.
             iEscolha = int( input( "Escolha o indice do livro que deseja pegar emprestado ou digite -1 para sair \n" ) )
-            
+
             # Nosso usuario deseja sair.
             if iEscolha == -1:
                 break
-            
+
             # Check para evitar indices invalidos.
             if iEscolha < 0 or iEscolha > iTamanhoDaLivraria:
                 print( "Por favor coloque um dos indices listado" )
                 continue
 
-            # Verifique se esse livro ja foi emprestado nao rode o codigo de emprestamento.
-            if lLivraria[ iEscolha ].bEmprestado:
-                print( f"Este livro ja foi empretado para: { lLivraria[ iEscolha ].strQuemAdquiriu }" )
-                continue
-            
             # Salve o usuario que vai pegar emprestado o livro.
             strNomeDeQuemVaiPegarEmprestado = input( "Digite o nome de quem vai pegar emprestado \n" )
-            
+
+            # Salve a idade do usuario que vai pegar emprestado o livro.
+            iIdadeDeQuemVaiPegarEmprestado = InputDeInteiro( "Digite a idade de quem vai pegar emprestado \n" )
+
             # Rode a funcao dur.
-            lLivraria[ iEscolha ].Emprestar( strNomeDeQuemVaiPegarEmprestado )
-            
+            bPare = lLivraria[ iEscolha ].Emprestar( strNomeDeQuemVaiPegarEmprestado, iIdadeDeQuemVaiPegarEmprestado )
+
             # Pare o loop ja atingimos nosso objeto.
-            break
+            if bPare:
+                break
 
         except Exception as e:
             # Pare o loop caso seja solicitado um interrupt ( CTRL + C ).
             if KeyboardInterrupt == e:
                 break
-            
+
             # Se acontecer outra coisa.
             print("Por favor digite um numero")
 
 def DevolverLivroNaLivraria( ):
-    print(pf.figlet_format( "Devolver livro" ) )
+    print( pf.figlet_format( "Devolver livro" ) )
 
     # Mostre os livros que tem na livraria.
     MostrarLivrosNaLivraria( lLivraria )
@@ -259,6 +284,9 @@ def DevolverLivroNaLivraria( ):
     if iTamanhoDaLivraria <= 0:
         print( "A livraria ta vazia, por favor adicione algum livro" )
         pass
+
+    # Pare o loop broski.
+    bPare = False
 
     # Parte da devolucao.
     while True:
@@ -273,43 +301,34 @@ def DevolverLivroNaLivraria( ):
             # Nosso usuario deseja sair.
             if iEscolha == -1:
                 break
-            
+
             # Check para evitar indices invalidos.
             if iEscolha < 0 or iEscolha > iTamanhoDaLivraria:
                 print( "Por favor coloque um dos indices listado" )
                 continue
 
-            # Verifique se esse livro nao foi emprestado se nao foi nao rode o resto.
-            if not lLivraria[ iEscolha ].bEmprestado:
-                print( f"Este livro ja esse livro nao foi emprestado" )
-                continue
-            
             # Salve o usuario que talvez tinha pego o livro.
             strNomeDeQuemTalvezTenhaPegadoEmprestado = input( "Digite o nome de quem vai devolver o livro \n" )
-            
-            # Verifique se foi esse usuario que pegou emprestado.
-            if strNomeDeQuemTalvezTenhaPegadoEmprestado != lLivraria[ iEscolha ].strQuemAdquiriu:
-                print( "Esse usuario nao pegou o livro" )
-                continue
 
             # Rode a funcao dur.
-            lLivraria[ iEscolha ].Devolver( strNomeDeQuemTalvezTenhaPegadoEmprestado )
-            
+            bPare = lLivraria[ iEscolha ].Devolver( strNomeDeQuemTalvezTenhaPegadoEmprestado )
+
             # Pare o loop ja atingimos nosso objeto.
-            break
+            if bPare:
+                break
 
         except Exception as e:
             # Pare o loop caso seja solicitado um interrupt ( CTRL + C ).
             if KeyboardInterrupt == e:
                 break
-            
+
             # Se acontecer outra coisa.
             print("Por favor digite um numero")
 
 while True:
     print( pf.figlet_format( "livraria do leo" ) )
     print( "1.adicionar livro                  2.remover livro\n3.emprestimo de livro              4.devolver\n\n\n" )
-    
+
     try:
         # Salve a escolha do usuario.
         iEscolha = int( input( "" ) )
@@ -338,7 +357,7 @@ while True:
             case 4:
                 LimparConsole( )
                 DevolverLivroNaLivraria( )
-            
+
             # Caso se o numero nao for nenhum dos anteriores.
             case _:
                 # braia: por o default: do python e case _ que porra e essa.
@@ -350,12 +369,3 @@ while True:
             break
 
         print( "Digite um numero por favor" )
-
-
-
-
-
-
-
-
-
